21b073edff5f0b26daa148634ba57ab875b41e5a
parisasledge-source <parisasledge@me.com>
2022-04-22 11:59:17 -0400

    Create Spring Boot MVC project

---
+++ PetSleuth/src/main/java/com/skilldistillery/petsleuth/PetSleuthApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.petsleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSleuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetSleuthApplication.class, args);
+	}
+
+}
---
+++ PetSleuth/src/test/java/com/skilldistillery/petsleuth/PetSleuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.petsleuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSleuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
