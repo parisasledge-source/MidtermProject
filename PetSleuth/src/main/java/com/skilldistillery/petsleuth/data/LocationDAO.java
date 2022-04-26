7e63b0162dc54a8ad4de10b88e8829ceb0ca6b98
Zachary Breitnauer <zbreitnauer@live.com>
2022-04-26 11:00:49 -0600

    added the active field in the database

--- PetSleuth/src/main/java/com/skilldistillery/petsleuth/data/LocationDAO.java
+++ PetSleuth/src/main/java/com/skilldistillery/petsleuth/data/LocationDAO.java
@@ -1,6 +1,19 @@
 package com.skilldistillery.petsleuth.data;
 
+
+import com.skilldistillery.petsleuth.entities.Location;
+
+
+
+
 public interface LocationDAO {
 	
-
+	Location findLocationById(int locationId);
+	
+	Location createNewLocation(Location location);
+	
+	Location findExistingLocation(String city, String state);
+	
+	Location updateLocation(int locationId, Location location);
+	
 }
