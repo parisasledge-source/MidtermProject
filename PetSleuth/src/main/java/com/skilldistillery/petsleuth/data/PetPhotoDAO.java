df355106e0ec3183a3726578c98c463e53c64233
parisasledge-source <parisasledge@me.com>
2022-04-27 15:26:54 -0400

    Checking

--- PetSleuth/src/main/java/com/skilldistillery/petsleuth/data/PetPhotoDAO.java
+++ PetSleuth/src/main/java/com/skilldistillery/petsleuth/data/PetPhotoDAO.java
@@ -1,5 +1,19 @@
 package com.skilldistillery.petsleuth.data;
 
+import java.util.List;
+
+import com.skilldistillery.petsleuth.entities.PetPhoto;
+
 public interface PetPhotoDAO {
+
+	
+	List<PetPhoto> findByUserId(int userId);
+
+//	public boolean hide(Integer id);
+	
+	
+	public void removePetPhoto(Integer petPhotoId);
+
+	PetPhoto updatePetPhoto(int petPhotoId, PetPhoto petPhoto);
 
 }
