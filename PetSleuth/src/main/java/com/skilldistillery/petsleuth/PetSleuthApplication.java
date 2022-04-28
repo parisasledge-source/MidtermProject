5689216c69087f2d02550f58f8e5dedece9403f6
DustinMC1802 <dustinmcollier@gmail.com>
2022-04-28 13:00:20 -0700

    Configured app for deployment

--- PetSleuth/src/main/java/com/skilldistillery/petsleuth/PetSleuthApplication.java
+++ PetSleuth/src/main/java/com/skilldistillery/petsleuth/PetSleuthApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.petsleuth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PetSleuthApplication {
+public class PetSleuthApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(PetSleuthApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetSleuthApplication.class, args);
